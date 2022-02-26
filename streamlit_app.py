fce534b404adde82e34605115a67ae8cc7da3d6a
Jieni-Yan <jennyyan54@gmail.com>
2022-02-25 19:39:27 -0800

    add firestore key and update test program

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,17 @@
 # @Software: PyCharm
 
 import streamlit as st
+from google.cloud import firestore
 
-st.header('Hello 🌎!')
+# Authenticate to Firestore with the JSON account key.
+db = firestore.Client.from_service_account_json("firestore-key.json")
 
-if st.button('Balloons?'):
-    st.balloons()
+# Create a reference to the Google post.
+doc_ref = db.collection("Tuition").document("Tuition")
+
+# Then get the data at that reference.
+doc = doc_ref.get()
+
+# Let's see what we got!
+# st.write("The id is: ", doc.id)
+st.write("The contents are: ", doc.to_dict())
