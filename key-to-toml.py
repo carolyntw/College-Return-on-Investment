96aa4f27d4721785b7f484d86740567db31480eb
Jieni-Yan <jennyyan54@gmail.com>
2022-02-25 21:14:29 -0800

    Covert JSON key into a secure way"

---
+++ key-to-toml.py
@@ -0,0 +1,18 @@
+# *-* coding:utf8 *_*
+# @Time: 2/25/22 20:57
+# @Author: Yan
+# @File: key-to-toml.py
+# @Software: PyCharm
+
+import toml
+
+output_file = "secrets.toml"
+
+with open("firestore-key.json") as json_file:
+    json_text = json_file.read()
+
+config = {"textkey": json_text}
+toml_config = toml.dumps(config)
+
+with open(output_file, "w") as target:
+    target.write(toml_config)
--- streamlit_app.py
+++ streamlit_app.py
@@ -6,12 +6,20 @@
 
 import streamlit as st
 from google.cloud import firestore
+import json
+
+
 
 # Authenticate to Firestore with the JSON account key.
 db = firestore.Client.from_service_account_json("firestore-key.json")
 
+key_dict = json.loads(st.secrets["textkey"])
+creds = service_account.Credentials.from_service_account_info(key_dict)
+db = firestore.Client(credentials=creds, project="streamlit-reddit")
+
+
 # Create a reference to the Google post.
-doc_ref = db.collection("Tuition").document("Tuition")
+# doc_ref = db.collection("Tuition").document("Tuition")
 
 # Then get the data at that reference.
 doc = doc_ref.get()
