890885a0a32337daca39aab19db1bf285676c055
Jieni-Yan <jennyyan54@gmail.com>
2022-03-20 13:46:14 -0700

    uplpoad json files to firestore

--- college-return-on-investment.py
+++ college-return-on-investment.py
@@ -8,8 +8,8 @@ import streamlit as st
 from google.cloud import firestore
 import json
 from google.oauth2 import service_account
-import sys
-
+import os
+import json
 
 # Authenticate to Firestore with the JSON account key.
 # db = firestore.Client.from_service_account_json("firestore-key.json")
@@ -17,6 +17,21 @@ import sys
 key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
 db = firestore.Client(credentials=creds, project="college-return-on-investment")
+
+file_dict = {"degrees-that-pay-back.json":"Undergraduate Major", "salaries-by-college-type.json": "School Name",
+            "salary_potential.json": "name", "tuition_cost.json":"name"}
+
+for filename in os.listdir('data'):
+    if filename.endswith('.json'):
+        collectionName = filename.split('.')[0] # filename minus ext will be used as collection name
+        f = open('data/' + filename, 'r')
+        docs = json.loads(f.read())
+        for doc in docs:
+            id = doc.pop(file_dict(filename), None)
+            if id:
+                db.collection(collectionName).document(id).set(doc, merge=True)
+            else:
+                db.collection(collectionName).add(doc)
 
 
 navi = st.sidebar.radio("Navigation", ["Home", "Data Display", "Contact Us"])
