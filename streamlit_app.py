1b4bde06c53b7507dbb60558cc2edfd41a1c6f90
Jieni-Yan <jennyyan54@gmail.com>
2022-03-20 20:14:43 -0700

    update main file

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,6 @@ from google.cloud import firestore
 import json
 from google.oauth2 import service_account
 
-
-
 # Authenticate to Firestore with the JSON account key.
 # db = firestore.Client.from_service_account_json("firestore-key.json")
 
@@ -38,7 +36,7 @@ if navi == "Home":
 
 if navi == "Data Display":
     # Create a reference to the Google post.
-    # doc_ref = db.collection("Tuition").document("Tuition")
+    doc_ref = db.collection("tuition_cost").document("ASA College")
 
     form = st.form(key="school_info")
     with form:
@@ -55,11 +53,11 @@ if navi == "Data Display":
         submitted = st.form_submit_button(label="Submit")
 
     # Then get the data at that reference.
-    # doc = doc_ref.get()
+    doc = doc_ref.get()
 
     # Let's see what we got!
-    # st.write("The id is: ", doc.id)
-    # st.write("The contents are: ", doc.to_dict())
+    st.write("The id is: ", doc.id)
+    st.write("The contents are: ", doc.to_dict())
 
 if navi == "Contact Us":
     st.header('📝 Feedback')
