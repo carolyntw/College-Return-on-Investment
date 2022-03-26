f501e8fad1bc1b348108edf9c6d7e19fc5ab6cf9
carolyntw <87163239+carolyntw@users.noreply.github.com>
2022-03-25 19:54:29 -0700

    add function, modify code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,3 @@
-# *-* coding:utf8 *_*
-# @Time: 2/25/22 00:05
-# @Author: Yan
-# @File: streamlit_app.py
-# @Software: PyCharm
-
-
 from google.cloud import firestore
 from google.oauth2 import service_account
 
@@ -12,14 +5,14 @@ from google.oauth2 import service_account
 import streamlit as st
 import json
 import pandas as pd
+import numpy
+from st_aggrid import AgGrid, GridOptionsBuilder
+from st_aggrid.shared import GridUpdateMode
 
-# Authenticate to Firestore with the JSON account key.
-# db = firestore.Client.from_service_account_json("firestore-key.json")
 
 key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
 db = firestore.Client(credentials=creds, project="college-return-on-investment")
-
 
 navi = st.sidebar.radio("Navigation", ["Home", "Data Display", "Contact Us"])
 
@@ -44,39 +37,52 @@ if navi == "Data Display":
     df = pd.read_json('data/tuition_cost.json')
     df2 = pd.read_json('data/degrees-that-pay-back.json')
 
-    form = st.form(key="state_info")
-    with form:
-        cols = st.columns(1)
-        state_code = df['state_code'].unique()  
-        state_code_choice = cols[0].selectbox('Select your state:', state_code)
-        submitted = st.form_submit_button(label="Submit")
-
-    form2 = st.form(key="school_info")
-    with form2:
-        cols = st.columns(1)
-        university = df['name'].loc[df['state_code'] == state_code_choice].unique()  
-        university_choice = cols[0].selectbox('Select your university:', university)  
-        submitted = st.form_submit_button(label="Submit")
-
-    form3 = st.form(key="Major_info")
-    with form3:
-        cols = st.columns(1)
-        major = df2['Undergraduate Major'].unique()  
-        major_choice = cols[0].selectbox('Select your major:', major) 
-        submitted = st.form_submit_button(label="Submit")
+    state_code = df['state_code'].unique()  
+    state_code_choice = st.selectbox('Select your state:', state_code)
+    university = df['name'].loc[df['state_code'] == state_code_choice].unique()  
+    university_choice = st.selectbox('Select your university:', university)
 
     name_doc_ref = db.collection("tuition_cost").document(university_choice)
     name_doc = name_doc_ref.get()
+    data = pd.DataFrame.from_dict(name_doc.to_dict(), orient='index', columns=[university_choice])
+    data = data.transpose()
+    # st.dataframe(data)
+
+    # function source: https://share.streamlit.io/streamlit/example-app-interactive-table/main
+    def aggrid_interactive_table(df: pd.DataFrame):
+        """Creates an st-aggrid interactive table based on a dataframe.
+
+        Args:
+            df (pd.DataFrame]): Source dataframe
+
+        Returns:
+            dict: The selected row
+        """
+        options = GridOptionsBuilder.from_dataframe(
+            df, enableRowGroup=True, enableValue=True, enablePivot=True
+        )
+
+        options.configure_side_bar()
+
+        options.configure_selection("single")
+        selection = AgGrid(
+            df,
+            enable_enterprise_modules=True,
+            gridOptions=options.build(),
+            theme="light",
+            update_mode=GridUpdateMode.MODEL_CHANGED,
+            allow_unsafe_jscode=True,
+        )
+
+        return selection
+
+    selection = aggrid_interactive_table(df=data)
+    major = df2['Undergraduate Major'].unique()  
+    major_choice = st.selectbox('Select your major:', major) 
     major_doc_ref = db.collection("degrees-that-pay-back").document(major_choice)
     major_doc = major_doc_ref.get()
-
-    data = pd.DataFrame.from_dict(name_doc.to_dict(), orient='index', columns=[university_choice])
-    # data2 = pd.DataFrame.from_dict(major_doc.to_dict(), orient='index', columns=[major])
     data2 = pd.DataFrame.from_dict(major_doc.to_dict(), orient='index', columns=['major'])
-    st.dataframe(data)
     st.dataframe(data2)
-
-
 
 if navi == "Contact Us":
     st.header('📝 Feedback')
